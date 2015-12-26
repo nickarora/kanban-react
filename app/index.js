5fad4e29e3b752c406bc82614af50af13ff2cf01
Nick Arora <arora.nick@gmail.com>
2015-12-26 04:51:08 -0800

    add initial React components

--- app/index.js
+++ app/index.js
@@ -1,8 +1,13 @@
 require('./main.css');
 
-var component = require('./component');
-var app = document.createElement('div');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-document.body.appendChild(app);
+main();
 
-app.appendChild(component());
+function main() {
+  const app = document.createElement('div');
+  document.body.appendChild(app);
+  ReactDOM.render(<App />, app);
+}
