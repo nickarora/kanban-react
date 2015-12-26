51f3228bf179b9666de65dfc57ebf31a5e81f588
Nick Arora <arora.nick@gmail.com>
2015-12-26 15:46:11 -0800

    create App state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,24 +2,32 @@ import React from 'react';
 import uuid from 'node-uuid';
 import Notes from './Notes';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Webpack'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do Laundry'
-  }
-];
-
 export default class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do Laundry'
+        }
+      ]
+    };
+  }
+
   render() {
+    const notes = this.state.notes;
+
     return (
       <div>
         <Notes items={notes} />
