44774976026c13b9921bab61f6fa5423ebfd747c
Nick Arora <arora.nick@gmail.com>
2015-12-27 15:35:16 -0800

    implement attachToLane store method

--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -1,6 +1,7 @@
 import uuid from 'node-uuid';
 import alt from '../libs/alt';
 import LaneActions from '../actions/LaneActions';
+import NoteStore from './NoteStore';
 
 class LaneStore {
   constructor() {
@@ -16,6 +17,28 @@ class LaneStore {
     this.setState({
       lanes: lanes.concat(lane)
     });
+  }
+
+  attachToLane(laneId, noteId) {
+    if (!noteId) {
+      this.waitFor(NoteStore); // useful if working with a async db
+      noteId = NoteStore.getState().notes.slice(-1)[0].id; //last note
+    }
+
+    const lanes = this.lanes.map((lane) => {
+      if (lane.id === laneId) {
+        // if note isn't in the lane
+        if (lane.notes.indexOf(noteId) === -1) {
+          lane.notes.push(noteId);
+        } else {
+          console.warn('Already attached to lane! ', lanes);
+        }
+      }
+
+      return lane;
+    });
+
+    this.setState({lanes});
   }
 }
 
