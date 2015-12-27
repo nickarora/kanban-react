4ff16795e130620276b4908c6f0f057a464ef504
Nick Arora <arora.nick@gmail.com>
2015-12-27 12:13:43 -0800

    initialize persistant local storage

--- app/libs/persist.js
+++ app/libs/persist.js
@@ -4,10 +4,18 @@ export default function(alt, storage, storeName) {
   const finalStore = makeFinalStore(alt);
 
   try {
+    // bootstrap == restore data
     alt.bootstrap(storage.get(storeName));
   }
   catch(e) {
     console.error('Failed to bootstrap data', e);
   }
+
+  finalStore.listen(() => {
+    if (!storage.get('debug')) {
+      // snapshot == save data
+      storage.set(storeName, alt.takeSnapshot());
+    }
+  });
 
 }
