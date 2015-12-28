4003c4ff3607819810500d838fe2f6d951d8bee7
Nick Arora <arora.nick@gmail.com>
2015-12-27 22:07:53 -0800

    show blank spot indicating where to move

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -7,6 +7,9 @@ const noteSource = {
     return {
       id: props.id
     };
+  },
+  isDragging(props, monitor) {
+    return props.id === monitor.getItem().id;
   }
 };
 
@@ -22,8 +25,9 @@ const noteTarget = {
   }
 };
 
-@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
-  connectDragSource: connect.dragSource()
+@DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging() // map isDragging() state to isDragging prop
 }))
 @DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
   connectDropTarget: connect.dropTarget()
@@ -31,11 +35,17 @@ const noteTarget = {
 export default class Note extends React.Component {
 
   render() {
-    const {connectDragSource, connectDropTarget,
+    const {connectDragSource, connectDropTarget, isDragging,
       id, onMove, ...props} = this.props;
 
     return connectDragSource(connectDropTarget(
-      <li {...this.props}>{this.props.children}</li>
+      <li
+        style={{
+          opacity: isDragging ? 0 : 1
+        }}
+        {...this.props}>
+        {this.props.children}
+      </li>
     ));
   }
 
