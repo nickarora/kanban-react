01742d924aab473dfd5b7fae78a6df46624b4659
Nick Arora <arora.nick@gmail.com>
2015-12-27 21:16:09 -0800

    convert Note component to a wrapper component

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Editable from './Editable';
+import Note from './Note';
 
 export default class Notes extends React.Component {
   render() {
@@ -11,12 +12,12 @@ export default class Notes extends React.Component {
   // note onEdit's first argument is bound to note.id
   renderNote = (note) => {
     return (
-      <li className="note" key={note.id}>
+      <Note className="note" id={note.id} key={note.id}>
         <Editable
           value={note.task}
           onEdit={this.props.onEdit.bind(null, note.id)}
           onDelete={this.props.onDelete.bind(null, note.id)} />
-      </li>
+      </Note>
     );
   }
 }
